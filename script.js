fb840c6410f9669ea1ba9f2f2b970f6bc9e90e97
IdirX69 <adil.idirr@gmail.com>
2022-10-02 20:06:43 +0200

    Fixed conflict N1

--- script.js
+++ script.js
@@ -3,10 +3,59 @@
 const logoText = document.querySelector(".logo-text");
 const logoImg = document.querySelector(".rotate");
 
-logoText.addEventListener('mouseover', function () {
-    logoImg.classList.add("rotate-bis");
+logoText.addEventListener("mouseover", function () {
+  logoImg.classList.add("rotate-bis");
 });
 
-logoText.addEventListener('mouseleave', function () {
-    logoImg.classList.remove("rotate-bis");
-})
+logoText.addEventListener("mouseleave", function () {
+  logoImg.classList.remove("rotate-bis");
+});
+//section beer presentation start
+const turtleImg = document.querySelector(".turtle-img");
+const turtleP = document.querySelector(".turtle-paragraph");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 980) {
+    turtleImg.style.animation = "beer-anim normal 2s forwards";
+  } else {
+    turtleImg.style.animation = "none";
+  }
+  if (window.scrollY > 981) {
+    turtleP.style.animation = "beer-paragraph-anim normal 2s forwards";
+  } else {
+    turtleP.style.animation = "none";
+  }
+});
+
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
+
+  const size = Math.random() * 10 + "px";
+
+  bubble.style.height = size;
+  bubble.style.width = size;
+  bubble.style.top = Math.random() * 100 + 40 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+setInterval(bubbleMaker, 200);
+
+//Section beer presentation end
+//ANIMATION CAPSULE
+
+const logoText = document.querySelector(".logo-text");
+const logoImg = document.querySelector(".rotate");
+
+logoText.addEventListener("mouseover", function () {
+  logoImg.classList.add("rotate-bis");
+});
+
+logoText.addEventListener("mouseleave", function () {
+  logoImg.classList.remove("rotate-bis");
+});
