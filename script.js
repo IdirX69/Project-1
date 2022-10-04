29ee89cdbcfb36df264255635c9bd8173d64ce18
gaellegoyon <gaellegoyon.pro@gmail.com>
2022-10-04 13:35:02 +0200

    modification gaelle

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ logoText.addEventListener('mouseover', function () {
 logoText.addEventListener('mouseleave', function () {
     logoImg.classList.remove("rotate-bis");
 })
+
+
+
+//HAMBURGER
+
+const hamburgerButton = document.querySelector(".nav-toggler");
+
+const navigation = document.querySelector("nav");
+
+hamburgerButton.addEventListener("click", toggleNav);
+
+
+function toggleNav() {
+    hamburgerButton.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+
+//validation age
+
+const txt = document.querySelector('.text-verif');
+const bout1 = document.querySelector('.btn-1');
+const bout2 = document.querySelector('.btn-2');
+const hiddenDiv = document.querySelector('.hidden-div');
+const sectionAge = document.querySelector('.sec-age');
+
+hiddenDiv.style.display = "none";
+
+bout1.addEventListener("click", function (event) {
+    txt.innerHTML = "Vous n'avez pas l'âge requis pour accéder à ce site !"
+    sectionAge.style.display = "none";
+    hiddenDiv.style.display = "contents";
+});
+
+bout2.addEventListener("click", function (event) {
+    txt.innerHTML = "Vous n'avez pas l'âge requis pour accéder à ce site !"
+});
+
+
