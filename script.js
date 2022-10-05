5c10a154f4fbf4dd36614f5674879b3a854a35d1
gaellegoyon <gaellegoyon.pro@gmail.com>
2022-10-05 13:28:08 +0200

    add nav

--- script.js
+++ script.js
@@ -16,17 +16,38 @@ logoText.addEventListener('mouseleave', function () {
 
 //HAMBURGER
 
-const hamburgerButton = document.querySelector(".nav-toggler");
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-const navigation = document.querySelector("nav");
+  // toggle nav
 
-hamburgerButton.addEventListener("click", toggleNav);
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
 
 
-function toggleNav() {
-  hamburgerButton.classList.toggle("active");
-  navigation.classList.toggle("active");
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`
+      }
+    });
+
+    //Burger Animation 
+
+    burger.classList.toggle('toggle');
+
+
+
+  });
+
 }
+
+navSlide();
+
 
 
 //validation age
@@ -123,40 +144,40 @@ const right = document.querySelector('.right');
 
 const slider = document.querySelector('.slidercarousel');
 
-const indicatorParent = document.querySelector('.controlcarousel ul'); 
+const indicatorParent = document.querySelector('.controlcarousel ul');
 const indicators = document.querySelectorAll('.controlcarousel li');
 index = 0;    /*definition d'une position 0 de la slide visible*/
 
 
 
 indicators.forEach((indicator, i) => {
-    indicator.addEventListener('click', () => {
-      document.querySelector('.controlcarousel .selected').classList.remove('selected');/*déplace le statut "selected" d'un bouton a l'autre*/
-      indicator.classList.add('selected');
-      slider.style.transform = 'translateX(' + (i) * -25 + '%)';  
-      index = i;
-      
-    });
+  indicator.addEventListener('click', () => {
+    document.querySelector('.controlcarousel .selected').classList.remove('selected');/*déplace le statut "selected" d'un bouton a l'autre*/
+    indicator.classList.add('selected');
+    slider.style.transform = 'translateX(' + (i) * -25 + '%)';
+    index = i;
+
   });
+});
 
 
 
 
-  left.addEventListener('click', function() {
-    index = (index > 0) ? index -1 : 0; /*condition empechant d'utiliser le slider si l'on depasse pas le permier avis*/
-    document.querySelector('.controlcarousel .selected').classList.remove('selected'); /*déplace le statut "selected" d'un bouton a l'autre*/
-    indicatorParent.children[index].classList.add('selected'); /*synchonise l'action de la fleche et le status selected du bouton*/
-    slider.style.transform = 'translateX(' + (index) * -25 + '%)'; /*event et fonction pour glisser le slider de 25% car 
+left.addEventListener('click', function () {
+  index = (index > 0) ? index - 1 : 0; /*condition empechant d'utiliser le slider si l'on depasse pas le permier avis*/
+  document.querySelector('.controlcarousel .selected').classList.remove('selected'); /*déplace le statut "selected" d'un bouton a l'autre*/
+  indicatorParent.children[index].classList.add('selected'); /*synchonise l'action de la fleche et le status selected du bouton*/
+  slider.style.transform = 'translateX(' + (index) * -25 + '%)'; /*event et fonction pour glisser le slider de 25% car 
     4 avis utilisation concatenation*/
-  });
+});
 
 
-  right.addEventListener('click', function() {
-    index = (index < 4 - 1) ? index+1 : 3; /*condition empechant d'utiliser le slider si l'on depasse pas le dernier avis*/
-    document.querySelector('.controlcarousel .selected').classList.remove('selected'); /*déplace le statut "selected" d'un bouton a l'autre*/
-    indicatorParent.children[index].classList.add('selected'); /*synchonise l'action de la fleche et le status selected du bouton*/
-    slider.style.transform = 'translateX(' + (index) * -25 + '%)'; /*event et fonction pour glisser le slider de 25% car 
+right.addEventListener('click', function () {
+  index = (index < 4 - 1) ? index + 1 : 3; /*condition empechant d'utiliser le slider si l'on depasse pas le dernier avis*/
+  document.querySelector('.controlcarousel .selected').classList.remove('selected'); /*déplace le statut "selected" d'un bouton a l'autre*/
+  indicatorParent.children[index].classList.add('selected'); /*synchonise l'action de la fleche et le status selected du bouton*/
+  slider.style.transform = 'translateX(' + (index) * -25 + '%)'; /*event et fonction pour glisser le slider de 25% car 
     4 avis utilisation concatenation*/
-  });
+});
 
 
